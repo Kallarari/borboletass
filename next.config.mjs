4e2b789c464eb166079ab06beeae3759a7b240ce
kallarari <kallarari@gmail.com>
2024-08-02 13:57:51 -0300

    feature(PWA)adds pwa to project

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,12 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+import withPWAInit from "next-pwa";
 
-export default nextConfig;
+const withPWA = withPWAInit({
+  dest: "public",
+});
+export default withPWA({
+    reactStrictMode:true,
+    disable: process.env.NODE_ENV === "development",
+    dest:"public",
+    register: true,
+    skipWaiting: true
+})
